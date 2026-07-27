cf5c6f69b3b212c455d8217ce9dee4118ea96aef
Yashvskothari <yashkothari2605@gmail.com>
2026-07-27 13:30:15 +0530

    backend deployed

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
